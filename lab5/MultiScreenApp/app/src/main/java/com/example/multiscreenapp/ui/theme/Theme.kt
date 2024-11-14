e9b68ff6bfd455d3984b5d0b22abb36ac8bf1681
anisort <jaki.anisort@gmail.com>
2024-11-14 16:05:02 +0200

    Update lab5

--- lab5/MultiScreenApp/app/src/main/java/com/example/multiscreenapp/ui/theme/Theme.kt
+++ lab5/MultiScreenApp/app/src/main/java/com/example/multiscreenapp/ui/theme/Theme.kt
@@ -9,42 +9,34 @@ import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
-private val LightBackgroundColor = Color(0xFFFFFBFE)
-private val DarkBackgroundColor = Color(0xFF121212)
-
-private val LightTextColor = Color(0xFF000000)
-private val DarkTextColor = Color(0xFFFFFFFF)
-
-private val LightPrimaryColor = Color(0xFF3700B3)
-private val DarkPrimaryColor = Color(0xFFBB86FC)
-
 private val DarkColorScheme = darkColorScheme(
-    primary = DarkPrimaryColor,
-    background = DarkBackgroundColor,
-    onPrimary = DarkTextColor,
-    onSecondary = DarkTextColor,
-    onTertiary = DarkTextColor,
-    onBackground = DarkTextColor,
-    onSurface = DarkTextColor
+    primary = Purple80,
+    secondary = PurpleGrey80,
+    tertiary = Pink80
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = LightPrimaryColor,
-    background = LightBackgroundColor,
-    onPrimary = LightTextColor,
-    onSecondary = LightTextColor,
-    onTertiary = LightTextColor,
-    onBackground = LightTextColor,
-    onSurface = LightTextColor
-)
+    primary = Purple40,
+    secondary = PurpleGrey40,
+    tertiary = Pink40
 
+    /* Other default colors to override
+    background = Color(0xFFFFFBFE),
+    surface = Color(0xFFFFFBFE),
+    onPrimary = Color.White,
+    onSecondary = Color.White,
+    onTertiary = Color.White,
+    onBackground = Color(0xFF1C1B1F),
+    onSurface = Color(0xFF1C1B1F),
+    */
+)
 
 @Composable
 fun MultiScreenAppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
+    // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
     content: @Composable () -> Unit
 ) {
@@ -53,6 +45,7 @@ fun MultiScreenAppTheme(
             val context = LocalContext.current
             if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
         }
+
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
